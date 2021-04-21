556ff57797df89c69c15441b11b5cc309b4d813d
BlueDiU <reycoc2311@gmail.com>
2021-04-21 13:28:50 -0600

    nested selects, done

--- src/components/NestedSelects.js
+++ src/components/NestedSelects.js
@@ -7,13 +7,15 @@ function NestedSelects() {
   const [town, setTown] = useState('');
   const [suburb, setSuburb] = useState('');
 
+  const TOKEN = 'd81a7ac7-976d-4e1e-b7d3-b1979d791b6c';
+
   return (
     <div className="container">
       <h3>México</h3>
 
       <SelectList
-        title="Estados"
-        url=""
+        title="estado"
+        url={`https://api-sepomex.hckdrk.mx/query/get_estados?token=${TOKEN}`}
         handleChange={(e) => {
           setState(e.target.value);
         }}
@@ -21,8 +23,8 @@ function NestedSelects() {
 
       {state && (
         <SelectList
-          title="Municipios"
-          url=""
+          title="municipios"
+          url={`https://api-sepomex.hckdrk.mx/query/get_municipio_por_estado/${state}?token=${TOKEN}`}
           handleChange={(e) => {
             setTown(e.target.value);
           }}
@@ -31,12 +33,23 @@ function NestedSelects() {
 
       {town && (
         <SelectList
-          title="Colonias"
-          url=""
+          title="colonia"
+          url={`https://api-sepomex.hckdrk.mx/query/get_colonia_por_municipio/${town}?token=${TOKEN}`}
           handleChange={(e) => {
             setSuburb(e.target.value);
           }}
         />
+      )}
+
+      {state ? (
+        <div>
+          <h5>Haz escogido</h5>
+          <p>{state}</p>
+          <p>{town}</p>
+          <p>{suburb}</p>
+        </div>
+      ) : (
+        <h5>No hay nada seleccionado</h5>
       )}
     </div>
   );
--- src/components/selects/SelectList.jsx
+++ src/components/selects/SelectList.jsx
@@ -1,18 +1,47 @@
 import React from 'react';
+import { useFetch } from '../../hooks/useFetch';
+import Loader from '../generics/Loader';
+import Message from '../generics/Message';
 
-function SelectList({ title }) {
+function SelectList({ title, url, handleChange }) {
+  const { data, error, loading } = useFetch(url);
+
+  if (!data) return null;
+
+  if (error) {
+    <Message
+      msg={`Error ${error.status}: ${error.statusText}`}
+      bgColor="#CB4335"
+    />;
+  }
+
+  let id = `select-${title}`;
+  let label = title.charAt(0).toUpperCase() + title.slice(1);
+
+  let options = data.response[title];
+
   return (
     <>
-      <label htmlFor="selectInput" className="d-block text-left">
-        {title}
+      <label htmlFor={id} className="d-block text-left">
+        {label}
       </label>
 
+      {loading && <Loader />}
+
       <select
-        className="form-control w-25 mb-3"
-        name=""
-        id="selectInput"
+        className="form-control mb-3"
+        name={id}
+        id={id}
+        onChange={handleChange}
+        style={{ cursor: 'pointer' }}
       >
-        <option value="">---</option>
+        <option value="">Elige un {title}</option>
+        {data &&
+          options.map((el) => (
+            <option key={el} value={el}>
+              {el}
+            </option>
+          ))}
       </select>
     </>
   );
